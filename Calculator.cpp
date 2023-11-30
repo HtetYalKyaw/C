6575be05da69e57aa7c89fb1f8e956371070cd4b
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2023-11-30 12:35:35 +0630

    Calc

---
+++ Calculator.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    cout << "*******************Calculator*******************\n";
+
+    cout << "Enter operator (+ - * / ):";
+    cin >> op;
+
+    cout << "Enter Number 1 :";
+    cin >> num1;
+
+    cout << "Enter Number 2 :";
+    cin >> num2;    
+
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        cout << "Result :" << result << "\n";
+        break;
+    case '-':
+        result = num1 - num2;
+        cout << "Result : " << result << "\n";
+        break;
+    case '*':
+        result = num1 * num2;
+        cout << "Result : " << result << "\n";
+        break;
+    case '/':
+        result = num1 / num2;
+        cout << "Result : " << result << "\n";
+        break;
+    default :
+        cout << "Invalid Operators. ";
+        break;
+    }
+
+cout << "************************************************";
+
+
+}
