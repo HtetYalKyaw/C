c1df358721d6eecba43ba04d076cbcac3bc2a3b4
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2023-12-12 15:50:53 +0630

    2

--- stringmethod.cpp
+++ stringmethod.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
 using namespace std;
-
-int main(){
     //name.length()
     //name.empty()
-
+    //name.clear()
+    //name.append()
+    //name.at() use to point number position
+    //name.insert(0, "T") use to replace number
+int main() {
     string name;
-    cout << "Enter Your Name : ";
+    cout << "Enter Your Name: ";
     getline(cin, name);
 
-    if (name.length()> 32 ){
-        cout << "You can't exceed 32 characters.";
-    }else{
-        cout << "Welcome, "<< name;
-    }
-
-    if (name.empty()){
+    if (name.empty()) {
         cout << "Please enter characters.";
-    }else{
-        return 0;
-
+    } else {
+        if (name.length() > 32) {
+            cout << "You can't exceed 32 characters.";
+        } else {
+            cout << "Welcome, " << name;
+        }
     }
+
     return 0;
 }
