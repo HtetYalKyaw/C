0e0a76f167f5c5dfd91f445ced1818d78bdf83f1
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2023-12-17 12:27:29 +0630

    oop

--- oop.cpp
+++ oop.cpp
@@ -3,62 +3,63 @@
 using namespace std;
 
 class Employee{
-    public:
+private:
     string Name;
     string Profession;
-    int age;
+    int Age;
+
+public:
+    void setName(string name){ //setter method 
+        Name = name;
+    }
+    string getName(){  //getter method 
+        return Name;
+    }
+
+    void setProfession(string profession){
+        Profession = profession;
+    }
+    string getProfession(){
+        return Profession;
+    }
+
+    void setAge (int age){
+        Age = age;
+    }
+    int getAge(){
+        return Age;
+    }
 
     void IntroduceYourSelf ()
     {
         cout << "Name -" << Name << endl;
         cout << "Profession -" << Profession << endl;
-        cout << "age -" << age << endl;        
+        cout << "age -" << Age << endl;        
     }
+    Employee(string name, string profession, int age){
+        Name = name;
+        Profession = profession;
+        Age = age;
+    }
+
 };
 
-bool isValidInput(const string& input){
-    for (char c :input){
-        if(!isspace(c)){
-        return true;
-        }
-        return false;
-    
-        }
-    }
-int main (){
+int main(){
 
-    Employee employee1;
-
-    cout << "Enter Your Name : "<< endl;
-    do{    
-        getline(cin,employee1.Name);
-        if(!isValidInput(employee1.Name)){
-            cout << "Please Enter a Valid Word: ";
-        }
-    
-    }while (!isValidInput(employee1.Profession));
-  
-    cout << "Enter Your Profession : "<< endl;
-    do{    
-        getline(cin,employee1.Profession);
-        if(!isValidInput(employee1.Profession)){
-            cout << "Please Enter a Valid Word: ";
-        }
-    
-    }while (!isValidInput(employee1.Profession));
-
-    cout << "Enter Your age : "<< endl;
-    do{    
-        getline(cin,employee1.age);
-        if(!isValidInput(employee1.age)){
-            cout << "Please Enter a Valid number: ";
-        }
-    
-    }while (!isValidInput(employee1.age));
-  
-
+    Employee employee1("Htet Yal Kyaw", "Unreal Engine", 24);
     employee1.IntroduceYourSelf();
+  
 
-return 0;
+    Employee emplyoee2("Shin Min Thwe", "HYK's wife", 24);
+    emplyoee2.IntroduceYourSelf();
+
+    employee1.setAge(26);
+    cout << employee1.getName() << " is " << employee1.getAge() << " Years old.";
+   
+    emplyoee2.setProfession("HYK's bitch.");
+    cout << emplyoee2.getName() << " is " << employee1.getName() << "'s " << emplyoee2.getProfession()<< endl;
+
+
+
+
 }
-
