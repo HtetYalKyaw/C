25086c1532a2def2c0fbcec2f85b1281a13ae302
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2023-12-24 12:05:13 +0630

    24/12/2023

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    // double Prices[] = {2.55, 3.56, 5.99};
+
+    // cout << Prices[0 ] << "\n";
+    
+    double prices [5];
+
+    prices[0] = 1.00;
+    prices[1] = 3.56;
+    prices[2] = 5.66;
+    prices[3] = 4.55;
+    prices[4] = 3.88;
+
+    cout << prices [2] << "\n";
+    return 0;
+
+}
