2494613c1e52d809de80fdda2a5be3a43b2d59e3
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-01-01 15:22:42 +0630

    1/1/2023

---
+++ 2darray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() 
+{
+   array<array<string, 3>, 3> cars = {{{"Mustang", "Escape", "F-150"},{"Corvette", "Equinox", "Silverado"},
+                                            {"Challenger", "Durango", "Ram 1500"}}};
+      
+
+    for (const auto& row : cars) {
+        for (const auto& model : row) {
+            std::cout << model << " ";
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
