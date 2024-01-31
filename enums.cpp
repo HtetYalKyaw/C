016636386246f31f9785309badf9f96d88e6e87d
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-01-31 19:48:08 +0630

    commit

---
+++ enums.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+enum Day{sunday = 0, monday = 1, tuesday = 2,wednesday = 3, thursday = 4, friday = 5, saturday = 6};
+
+
+int main(){
+
+    Day today = friday;
+
+    switch (today)
+    {
+        case sunday : cout << "This is sunday" << endl; 
+            break;
+        case monday : cout << "This is Monday" << endl; 
+            break;
+        case tuesday : cout << "This is tuesday" << endl; 
+            break;
+        case wednesday : cout << "This is wednesday" << endl; 
+            break;
+        case thursday : cout << "This is thursday" << endl; 
+            break;
+        case friday : cout << "This is friday" << endl; 
+            break;
+        case saturday : cout << "This is saturday" << endl; 
+            break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
