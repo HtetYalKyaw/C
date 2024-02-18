df895cc1a9cc61ad32f823aec3e0bea81814a8ad
HtetYalKyaw <htetyeekyaw2015@gmail.com>
2024-02-18 22:56:54 +0630

    18/2/2024

--- exercise.cpp
+++ exercise.cpp
@@ -4,51 +4,89 @@
 using namespace std;
 
 int AttackHero(int HeroDamage, int &enemyHP, double herocritchance, double herocritMultiplier);
-int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritMultiplier);
-int heroHeal(int &potionleft, int &potion,int &heroHP);
+int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritmultiplier);
+int heroHeal(int &potionleft, int &heal,int &heroHP);
+void Encounter(int &HeroDamage, int &heroHP, double herocritchance, double herocritMultiplier,int enemydamage,int &enemyHP , double enemycritchance, double enemycritmultiplier,int &potionleft, int &heal);
+
 int main(){
     srand(time(0));
     char temp;
-    int HeroDamage = 6;
+    int HeroDamage = 5;
     int enemydamage = 5;
-    int enemyHP = 300;
+    int enemyHP = 200;
     int heroHP = 100;
-    int potionleft = 2;
+    int potionleft = 1;
     double herocritchance = 0.35; 
     double herocritMultiplier = 0.70;
     double enemycritchance = 0.20;
     double enemycritmultiplier = 0.60;
-    int potion = 100 ;
+    int heal = 100 ;
 
- 
+    
+    while(enemyHP > 0 ){
+    cout << "You stand at a crossroad ! Choose Left (L) or Right (R) :";
+    cin >> temp;
 
-    while(heroHP > 0 && enemyHP > 0){
-        cout << "Attack(A) or Potion(P) :";
-        cin >> temp;
-
-        if (temp == 'a' || temp == 'A'){
-            AttackHero(HeroDamage, enemyHP, herocritchance, herocritMultiplier);
-        }else if (temp == 'p' || temp == 'P'){
-            heroHeal(potionleft ,potion, heroHP);
-        }else{
-            cout << "Invalid Input !";
-        }
-        if (enemyHP <= 0){
-            cout << "Congradulatiion, the enemy has perished !"<< endl;
-            break;
-        }
-
-        AttackEnemy(enemydamage, heroHP, enemycritchance, enemycritmultiplier);
-
-        if (heroHP <= 0){
-            cout << "Game Over" << endl;
-            break;
-        }
+    if (temp == 'l' || temp == 'L' ){
+        cout << "You choose Left ." << endl;
+        Encounter(HeroDamage, heroHP, herocritchance, herocritMultiplier, enemydamage, enemyHP ,enemycritchance, enemycritmultiplier, potionleft, heal);
+    }else if (temp == 'r' || temp == 'R'){
+        cout << "You choose Right ."<< endl;
+        Encounter(HeroDamage, heroHP, herocritchance, herocritMultiplier, enemydamage, enemyHP , enemycritchance, enemycritmultiplier, potionleft, heal);
+    }else{
+        cout << "Invalid Input ."<<endl;
+    }
 
     }
+    cout << "All Enemy has perished . You saved the world."<< endl;
+
     return 0;
 
 }
+void Encounter(int &HeroDamage, int &heroHP, double herocritchance, double herocritMultiplier,int enemydamage,int &enemyHP , double enemycritchance, double enemycritmultiplier,int &potionleft, int &heal){
+
+    char encounter = rand() % 4 + 1 ;
+    char temp;
+        switch (encounter)
+        {   
+        case 1: cout << "Nothing Happened ! Your Journey continue !"<< endl;
+            break;
+        case 2: cout << "You found a chest . You opened and received a damage buff. " << endl;
+            HeroDamage++;
+            break;
+        case 3: cout << "You found a chest . You opened and received a potion. " << endl;
+            potionleft++;
+            break;
+        case 4: cout << "YOU HAVE ENCOUNTERED AN ENEMY. " << endl; 
+            while(heroHP > 0 && enemyHP > 0){
+                cout << "Attack( A ) or Potion( P ) :";
+                cin >> temp;
+
+                if (temp == 'a' || temp == 'A'){
+                    AttackHero(HeroDamage, enemyHP, herocritchance, herocritMultiplier);
+                }else if (temp == 'p' || temp == 'P'){
+                    heroHeal(potionleft ,heal, heroHP);
+                }else{
+                    cout << "Invalid Input !";
+                }
+                if (enemyHP <= 0){
+                    cout << "Congradulatiion, the enemy has perished !"<< endl;
+                }
+                    AttackEnemy(enemydamage, heroHP, enemycritchance, enemycritmultiplier);
+
+                if (heroHP <= 0){
+                    cout << "Game Over" << endl;
+                break;
+                }
+
+            }break;
+
+            default: break;
+        }
+    
+
+}
+
 int AttackHero(int HeroDamage, int &enemyHP,double herocritchance, double herocritMultiplier){
 
     bool isCritical = (rand()/ static_cast<double>(RAND_MAX)) < herocritchance;
@@ -65,11 +103,11 @@ int AttackHero(int HeroDamage, int &enemyHP,double herocritchance, double herocr
     return enemyHP;
 }
 
-int heroHeal(int &potionleft, int &potion, int &heroHP){
+int heroHeal(int &potionleft, int &heal, int &heroHP){
     if (potionleft > 0){
-        int potion = 100;
-        heroHP = min(heroHP+potion, 100) ;
-        cout << "You Healed for "<< potion << " HP."<< endl;
+        int heal = 100;
+        heroHP = min(heroHP+heal, 100) ;
+        cout << "You Healed for "<< heal << " HP."<< endl;
         potionleft--;
         cout << potionleft<<" potion Left.";
     }else if(heroHP < 100){
@@ -83,14 +121,14 @@ int heroHeal(int &potionleft, int &potion, int &heroHP){
 }
 
 
-int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritMultiplier){
+int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritmultiplier){
 
         bool isCritical = (rand()/ static_cast<double>(RAND_MAX)) < enemycritchance;
 
         int Damagedealt = enemydamage;
 
         if (isCritical){
-            Damagedealt *= (1 + enemycritMultiplier);            
+            Damagedealt *= (1 + enemycritmultiplier);            
             cout << "Enemy score Critical hit! " <<endl;
         }
 
@@ -99,3 +137,6 @@ int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double ene
         
         return heroHP;
 }
+
+
+
--- gpt.cpp
+++ gpt.cpp
@@ -3,77 +3,128 @@
 
 using namespace std;
 
-int AttackHero(int HeroDamage, int &enemyHP, double criticalChance, double criticalDamageMultiplier);
-int AttackEnemy(int EnemyDamage, int &heroHP, double criticalChance, double criticalDamageMultiplier);
+int AttackHero(int HeroDamage, int &enemyHP, double herocritchance, double herocritMultiplier);
+int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritmultiplier);
+int heroHeal(int &potionleft, int &heroHP);
+void Encounter(int &HeroDamage, int &heroHP, double herocritchance, double herocritMultiplier, int enemydamage, int &enemyHP, double enemycritchance, double enemycritmultiplier, int &potionleft);
 
-int main (){
-    srand(time(0)); // Seed for random number generation
-
+int main() {
+    srand(time(0));
     char temp;
     int HeroDamage = 5;
+    int enemydamage = 5;
+    int enemyHP = 200;
     int heroHP = 100;
-    int EnemyDamage = 5;
-    int enemyHP = 100;
-    double criticalChance = 0.20;
-    double criticalDamageMultiplier = 0.60;
+    int potionleft = 1;
 
-    while (heroHP > 0 && enemyHP > 0) {
-        cout << "Press 'A' for hero to attack: ";
+    while (enemyHP > 0) {
+        cout << "You stand at a crossroad! Choose Left (L) or Right (R): ";
         cin >> temp;
-        if (temp == 'a' || temp == 'A') {
-            AttackHero(HeroDamage, enemyHP, criticalChance, criticalDamageMultiplier); 
+
+        if (temp == 'l' || temp == 'L') {
+            cout << "You choose Left." << endl;
+            Encounter(HeroDamage, heroHP, 0.35, 0.70, enemydamage, enemyHP, 0.20, 0.60, potionleft);
+        } else if (temp == 'r' || temp == 'R') {
+            cout << "You choose Right." << endl;
+            Encounter(HeroDamage, heroHP, 0.35, 0.70, enemydamage, enemyHP, 0.20, 0.60, potionleft);
         } else {
-            cout << "Invalid input. Please try again." << endl;
-        }
-
-        if (enemyHP <= 0) {
-            cout << "Congratulations, the enemy has perished." << endl;
-            break;
-        }
-
-        AttackEnemy(EnemyDamage, heroHP, criticalChance, criticalDamageMultiplier); 
-
-        if (heroHP <= 0) {
-            cout << "Hero has been defeated. Game over." << endl;
-            break;
+            cout << "Invalid Input." << endl;
         }
     }
+
+    cout << "All enemies have been defeated. You win!" << endl;
 
     return 0;
 }
 
-int AttackHero(int HeroDamage, int &enemyHP, double criticalChance, double criticalDamageMultiplier){
-    // Check for critical hit
-    bool isCritical = (rand() / static_cast<double>(RAND_MAX)) < criticalChance;
+void Encounter(int &HeroDamage, int &heroHP, double herocritchance, double herocritMultiplier, int enemydamage, int &enemyHP, double enemycritchance, double enemycritmultiplier, int &potionleft) {
+    char encounter = rand() % 4 + 1;
 
-    // Calculate damage
-    int damageDealt = HeroDamage;
+    switch (encounter) {
+        case 1:
+            cout << "Nothing happened! Your journey continues." << endl;
+            break;
+        case 2:
+            cout << "You found a chest. You opened it and received a damage buff." << endl;
+            HeroDamage++;
+            break;
+        case 3:
+            cout << "You found a chest. You opened it and received a potion." << endl;
+            potionleft++;
+            break;
+        case 4:
+            cout << "You have encountered an enemy." << endl;
+            while (heroHP > 0 && enemyHP > 0) {
+                cout << "Attack (A) or Potion (P): ";
+                cin >> encounter;
+
+                if (encounter == 'a' || encounter == 'A') {
+                    AttackHero(HeroDamage, enemyHP, herocritchance, herocritMultiplier);
+                } else if (encounter == 'p' || encounter == 'P') {
+                    heroHeal(potionleft, heroHP);
+                } else {
+                    cout << "Invalid Input!" << endl;
+                }
+
+                if (enemyHP <= 0) {
+                    cout << "Congratulations, the enemy has perished!" << endl;
+                    break;
+                }
+
+                AttackEnemy(enemydamage, heroHP, enemycritchance, enemycritmultiplier);
+
+                if (heroHP <= 0) {
+                    cout << "Game Over" << endl;
+                    break;
+                }
+            }
+            break;
+        default:
+            cout << "Unexpected encounter!" << endl;
+            break;
+    }
+}
+
+int AttackHero(int HeroDamage, int &enemyHP, double herocritchance, double herocritMultiplier) {
+    bool isCritical = (rand() / static_cast<double>(RAND_MAX)) < herocritchance;
+    int Damagedealt = HeroDamage;
+
     if (isCritical) {
-        damageDealt *= (1 + criticalDamageMultiplier);
-        cout << "Hero Critical Hit! ";
+        Damagedealt *= (1 + herocritMultiplier);
+        cout << "You scored a Critical hit!" << endl;
     }
 
-    // Apply damage to enemy
-    enemyHP = max(enemyHP - damageDealt, 0);
-    cout << "Enemy HP: " << enemyHP << endl;
+    enemyHP = max(enemyHP - Damagedealt, 0);
+    cout << "The Enemy HP is " << enemyHP << endl;
 
     return enemyHP;
 }
 
-int AttackEnemy(int EnemyDamage, int &heroHP, double criticalChance, double criticalDamageMultiplier){
-    // Check for critical hit
-    bool isCritical = (rand() / static_cast<double>(RAND_MAX)) < criticalChance;
-
-    // Calculate damage
-    int damageDealt = EnemyDamage;
-    if (isCritical) {
-        damageDealt *= (1 + criticalDamageMultiplier);
-        cout << "Enemy Critical Hit! ";
+int heroHeal(int &potionleft, int &heroHP) {
+    if (potionleft > 0) {
+        int potion = 100;
+        heroHP = min(heroHP + potion, 100);
+        cout << "You healed for " << potion << " HP." << endl;
+        potionleft--;
+        cout << potionleft << " potions left." << endl;
+    } else {
+        cout << "You have no potions left." << endl;
     }
 
-    // Apply damage to hero
-    heroHP = max(heroHP - damageDealt, 0);
-    cout << "Hero HP: " << heroHP << endl;
+    return heroHP;
+}
+
+int AttackEnemy(int enemydamage, int &heroHP, double enemycritchance, double enemycritmultiplier) {
+    bool isCritical = (rand() / static_cast<double>(RAND_MAX)) < enemycritchance;
+    int Damagedealt = enemydamage;
+
+    if (isCritical) {
+        Damagedealt *= (1 + enemycritmultiplier);
+        cout << "The enemy scored a Critical hit!" << endl;
+    }
+
+    heroHP = max(heroHP - Damagedealt, 0);
+    cout << "The enemy hit you. Your HP is " << heroHP << endl;
 
     return heroHP;
 }
